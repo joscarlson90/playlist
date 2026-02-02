370d2502cae7a5fd542c46e41c68c929e9eaa647
Josefine Carlson <josefinecarlson@MacBook-Air-som-tillhor-Josefine-2.local>
2026-02-02 19:05:10 +0100

    added first teststyling

---
+++ script.js
@@ -0,0 +1,14 @@
+const playlists = [
+  {
+    name: "Favourite",
+    genre: "Pop",
+    artist: "Justin Bieber",
+    songs: ["Sorry", "Love Yourself"],
+  },
+  {
+    name: "Chill",
+    genre: "Soul",
+    artist: "Aretha Franklin",
+    songs: ["Lady Soul", "Soul 69"]
+  },
+];
